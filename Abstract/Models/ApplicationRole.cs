65ef6f97d9aac62837abf8529e5b8508a35dc019
DESKTOP-1UR1491\Vladyslav <onekryvenko.vladyslav@gmail.com>
2023-07-21 22:41:15 +0300

    Added features for Abstract class library

---
+++ Abstract/Models/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Abstract.Models
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    { }
+}
---
+++ Abstract/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Abstract.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set;}
+    }
+}
